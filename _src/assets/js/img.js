097a380fbb12725a0573b59165b895ffebe8eb4e
crod2 <carlarodriguez90@gmail.com>
2019-04-06 18:38:19 +0200

    Create img.js

---
+++ _src/assets/js/img.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const button = document.querySelector('.button__thumbnail');
+const photo = document.querySelector('.photo');
+const thumbnail = document.querySelector('.thumbnail__image');
+
+
+button.addEventListener('click', photoChange);
