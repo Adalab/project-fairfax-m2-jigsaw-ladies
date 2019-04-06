b7d27472c4204826ea65d769addfa20f89c20628
crod2 <carlarodriguez90@gmail.com>
2019-04-06 19:34:39 +0200

    Create color-preview.js

---
+++ _src/assets/js/img.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const button = document.querySelector('.button__thumbnail');
+const photo = document.querySelector('.photo');
+const thumbnail = document.querySelector('.thumbnail__image');
+
+function photoChange(event) {
+    
+}
+
+button.addEventListener('click', photoChange);
