8c86f2b233bf64078a4c30bc64d85dfd31e2e2f5
Alejandra Soto Arevalo <asoto89@gmail.com>
2019-04-08 23:30:12 +0200

    improve collapsible

--- _src/assets/js/collapsible.js
+++ _src/assets/js/collapsible.js
@@ -1,18 +1,16 @@
 'use strict';
 
 const designContainer = document.querySelector('.design__container');
-
-// const designContainer = document.querySelector('.design__main__container');
 const designCollapsible = document.querySelector('.design__collapsible')
 
-const fillContainer = document.querySelector('.fill__container__section');
+const fillContainer = document.querySelector('.fill__container');
 const fillCollapsible = document.querySelector('.fill__collapsible')
 
-const shareContainer = document.querySelector('.container__button');
+const shareContainer = document.querySelector('.share__container');
 const shareCollapsible = document.querySelector('.share__collapsible')
 
 function collapsible(containerToHide) {
-    containerToHide.classList.toggle('hidden__design');
+    containerToHide.classList.toggle('hidden__collapsible');
 }
 
 designCollapsible.addEventListener('click', function(){collapsible(designContainer)});
