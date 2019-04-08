451a8ea7281dce03e87fa6919d96474803b4656d
Alejandra Soto Arevalo <asoto89@gmail.com>
2019-04-08 23:17:36 +0200

    added rotate to arrow

--- _src/assets/js/collapsible.js
+++ _src/assets/js/collapsible.js
@@ -1,6 +1,8 @@
 'use strict';
 
-const designContainer = document.querySelector('.design__main__container');
+const designContainer = document.querySelector('.design__container');
+
+// const designContainer = document.querySelector('.design__main__container');
 const designCollapsible = document.querySelector('.design__collapsible')
 
 const fillContainer = document.querySelector('.fill__container__section');
@@ -10,7 +12,7 @@ const shareContainer = document.querySelector('.container__button');
 const shareCollapsible = document.querySelector('.share__collapsible')
 
 function collapsible(containerToHide) {
-    containerToHide.classList.toggle('hidden__collapsible');
+    containerToHide.classList.toggle('hidden__design');
 }
 
 designCollapsible.addEventListener('click', function(){collapsible(designContainer)});
