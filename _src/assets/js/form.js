b83854244f4f395caa84f686bc647efcbd5e55b9
crod2 <carlarodriguez90@gmail.com>
2019-04-15 11:43:33 +0200

    Resolve errors on form.js

--- _src/assets/js/form.js
+++ _src/assets/js/form.js
@@ -45,77 +45,52 @@ function previewCard(event, preview, text) {
 
 valueInputName.addEventListener('keyup', function() {previewCard(event, valueName, 'Nombre Apellido')});
 valueInputJob.addEventListener('keyup', function() {previewCard(event, valueJob, 'Front-end Developer')});
-valueInputTel.addEventListener('keyup', function() {previewCard(event, valueTel, `<a class="rs__element" href="tel:${event.currentTarget.value}"><i class="icon fas fa-mobile-alt"></i></a>`)});
-valueInputEmail.addEventListener('keyup', function() {previewCard(event, valueEmail, `<a class="rs__element" href="mailto:${value}"><i class="icon far fa-envelope"></i></a>`)});
-valueInputLinkedin.addEventListener('keyup', function() {previewCard(event, valueLinkedin, `<a class="rs__element" href="${value}"><i class="icon fab fa-linkedin-in"></i></a>`)});
-valueInputGithub.addEventListener('keyup', function() {previewCard(event, valueGithub, `<a class="rs__element" href="${value}" target="_blank"><i class="icon fab fa-github-alt"></i></a>`)});
+valueInputTel.addEventListener('keyup', previewCardTel);
+valueInputEmail.addEventListener('keyup', previewCardEmail);
+valueInputLinkedin.addEventListener('keyup', previewCardLinkedin);
+valueInputGithub.addEventListener('keyup', previewCardGithub);
 
-// function previewCardName() {
-//     if ( valueInputName.value === '') {
-//         valueName.innerHTML = "Nombre Apellido";
-//     } else {
-//         valueName.innerHTML = valueInputName.value;
-//     }
-// }
+function previewCardTel(event) {
+    const trigger = event.currentTarget;
+    const value = trigger.value;
 
-// function previewCardJob() {
-//     if ( valueInputJob.value === '') {
-//         valueJob.innerHTML = "Front-end Unicorn";
-//     } else {
-//         valueJob.innerHTML = valueInputJob.value;
-//     }
-// }
+    if ( value === '') {
+        valueTel.innerHTML = '';
+    } else {
+        valueTel.innerHTML = `<a class="rs__element" href="tel:${value}"><i class="icon fas fa-mobile-alt"></i></a>`;
+    }    
+}
 
+function previewCardEmail(event) {
+    const trigger = event.currentTarget;
+    const value = trigger.value;
 
+    if (value === '') {
+        valueEmail.innerHTML = '';
+    } else {
+        valueEmail.innerHTML = `<a class="rs__element" href="mailto:${value}">
+        <i class="icon far fa-envelope"></i></a>`;
+    }
+}
 
+function previewCardLinkedin (event) {
+    const trigger = event.currentTarget;
+    const value = trigger.value;
 
-// function previewCardTel(event) {
-//     const trigger = event.currentTarget;
-//     const value = trigger.value;
+    if (value === '') {
+        valueLinkedin.innerHTML = '';
+    } else {
+        valueLinkedin.innerHTML = `<a class="rs__element" href="${value}"><i class="icon fab fa-linkedin-in"></i></a>`;
+    }
+}
 
-//     if ( value === '') {
-//         valueTel.innerHTML = '';
-//     } else {
-//         valueTel.innerHTML = `<a class="rs__element" href="tel:${value}"><i class="icon fas fa-mobile-alt"></i></a>`;
-//     }    
-// }
+function previewCardGithub (event) {
+    const trigger = event.currentTarget;
+    const value = trigger.value;
 
-
-
-// function previewCardEmail(event) {
-//     const trigger = event.currentTarget;
-//     const value = trigger.value;
-
-//     if (value === '') {
-//         valueEmail.innerHTML = '';
-//     } else {
-//         valueEmail.innerHTML = `<a class="rs__element" href="mailto:${value}">
-//         <i class="icon far fa-envelope"></i></a>`;
-//     }
-// }
-
-
-
-// function previewCardLinkedin (event) {
-//     const trigger = event.currentTarget;
-//     const value = trigger.value;
-
-//     if (value === '') {
-//         valueLinkedin.innerHTML = '';
-//     } else {
-//         valueLinkedin.innerHTML = `<a class="rs__element" href="${value}"><i class="icon fab fa-linkedin-in"></i></a>`;
-//     }
-// }
-
-
-
-// function previewCardGithub (event) {
-//     const trigger = event.currentTarget;
-//     const value = trigger.value;
-
-//     if (value === '') {
-//         valueGithub.innerHTML = '';
-//     } else {
-//         valueGithub.innerHTML = `<a class="rs__element" href="${value}" target="_blank"><i class="icon fab fa-github-alt"></i></a>`;
-//     }
-// }
+    if (value === '') {
+        valueGithub.innerHTML = '';
+    } else {
+        valueGithub.innerHTML = `<a class="rs__element" href="${value}" target="_blank"><i class="icon fab fa-github-alt"></i></a>`;
+    }
+}
