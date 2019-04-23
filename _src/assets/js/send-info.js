5fd1258a865149eedbdd3a8428028c517a1e09b6
Marta Sobrino <marta.sobrino.web@gmail.com>
2019-04-23 20:53:56 +0200

    api link

--- _src/assets/js/send-info.js
+++ _src/assets/js/send-info.js
@@ -2,26 +2,38 @@
 
 const sendButton = document.querySelector('.share__button');
 const form = document.querySelector('form');
-const result = document.querySelector('.twitter__container--link');
+const urlLink = document.querySelector('.twitter__container--link');
+const urlContainer = document.querySelector('.twitter__container');
+
 
 function waitForIt() {
     event.preventDefault();
+    sendButton.classList.add('disabled__button');
+    sendButton.setAttribute('disabled', true);
     sendRequest();
 }
 
-sendButton.addEventListener('click', waitForIt);
 
-function sendRequest(json){
+
+function sendRequest(){
 
   fetch('https://us-central1-awesome-cards-cf6f0.cloudfunctions.net/card/', {
-    method: 'POST',
-    body: JSON.stringify(json),
-    headers: {
-      'content-type': 'application/json'
-    },
-  })
+      method: 'POST',
+      body: JSON.stringify(dataInfo),
+      headers: {
+        'content-type': 'application/json'
+      },
+    })
     .then(function(resp) { return resp.json(); })
     .then(function(result) { showURL(result); })
     .catch(function(error) { console.log(error); });
-}
+    }
 
+function showURL(result){
+  if(result.success){
+    urlLink.href = result.cardURL;
+    urlLink.innerHTML = result.cardURL;
+  }
+  urlContainer.classList.remove('hidden');
+}
+sendButton.addEventListener('click', waitForIt);
